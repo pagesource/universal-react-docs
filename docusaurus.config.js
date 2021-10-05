1f16ebaa9cb063b273a97825edee3fcb9a3bed8e
Sowmya Hemanthkumar <sowmihk@gmail.com>
2021-10-05 16:23:38 +0530

    Added blog tab

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,7 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
+        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/pagesource/universal-react-v2',
           label: 'GitHub',
